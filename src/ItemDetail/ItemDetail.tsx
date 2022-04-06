01b2f6d0ac6e208266203fb6524df99386256568
Komal Fulwar <47312905+komal-fulwar@users.noreply.github.com>
2022-04-07 03:03:35 +0530

    small changes

--- src/ItemDetail/ItemDetail.tsx
+++ src/ItemDetail/ItemDetail.tsx
@@ -2,20 +2,17 @@ import { useTheme } from "@mui/material/styles";
 
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
 import IconButton from "@mui/material/IconButton";
 
-import Typography from "@mui/material/Typography";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style.css";
 import Grid from "@mui/material/Grid";
 import { useParams } from "react-router-dom";
-import Header from "../header/Header";
 import { getNftDetails } from "../service/api";
 import { useEffect, useState } from "react";
-import { PhotoCamera } from "@material-ui/icons";
-import { fontSize } from "@mui/system";
+import InsertLinkOutlinedIcon from "@mui/icons-material/InsertLinkOutlined";
+import { Tooltip, Typography } from "@material-ui/core";
+import React from "react";
 
 export default function ItemDetail() {
   let { nftId } = useParams();
@@ -37,7 +34,7 @@ export default function ItemDetail() {
       <div className="container  " style={{ paddingTop: "2rem" }}>
         <div className="item_details">
           <div className="row justify-content-center sm:space-y-20">
-            <div className="col-lg-4">
+            <div className="col-lg-6">
               <img
                 className="item_img"
                 src={nftDetails?.image}
@@ -60,7 +57,7 @@ export default function ItemDetail() {
                     <div className="hr"></div>
                   </div>
                   <br></br>
-                  <Box>
+                  <Box className="web">
                     <Grid
                       container
                       rowSpacing={3}
@@ -68,6 +65,40 @@ export default function ItemDetail() {
                     >
                       {nftDetails?.rank_explain?.map((attribute: any) => (
                         <Grid item xs={4}>
+                          <Card
+                            style={{
+                              padding: "10px",
+                              backgroundColor: "#0b1b27",
+                            }}
+                          >
+                            <h6 style={{ color: "rgb(247 220 90)" }}>
+                              {attribute?.attribute}
+                            </h6>
+                            <span style={{ color: "white" }}>
+                              {" "}
+                              {attribute.value === "" ? "N/A" : attribute.value}
+                            </span>
+                            <br></br>
+
+                            <span
+                              style={{ color: "#FFEF7E", fontSize: "0.9rem" }}
+                            >
+                              {" "}
+                              {attribute.times_seen_per + "% Have This Trait "}
+                            </span>
+                          </Card>
+                        </Grid>
+                      ))}
+                    </Grid>
+                  </Box>
+                  <Box className="mobile">
+                    <Grid
+                      container
+                      rowSpacing={3}
+                      columnSpacing={{ xs: 1, sm: 3, md: 3 }}
+                    >
+                      {nftDetails?.rank_explain?.map((attribute: any) => (
+                        <Grid item xs={12}>
                           <Card
                             style={{
                               padding: "10px",
@@ -114,16 +145,39 @@ export default function ItemDetail() {
                           >
                             Mint Address
                           </p>
-                          <span
-                            style={{
-                              fontFamily: "Aldrich",
-                              color: "white",
-                              fontSize: "0.9rem",
-                            }}
-                            className="color_text"
+                          <Tooltip
+                            title={
+                              <span
+                                style={{
+                                  padding: "45px",
+                                  fontSize: "15px",
+                                  color: "white",
+                                }}
+                              >
+                                click and visit solscan
+                              </span>
+                            }
                           >
-                            {nftDetails.mint}
-                          </span>
+                            <a
+                              href={`https://solscan.io/token/${nftDetails.mint}`}
+                              style={{
+                                fontFamily: "Aldrich",
+                                color: "white",
+                                fontSize: "0.9rem",
+                                overflowWrap: "break-word",
+                              }}
+                              className="color_text"
+                            >
+                              {nftDetails.mint}
+                              <IconButton
+                                aria-label="upload picture"
+                                component="span"
+                                style={{ color: "white" }}
+                              >
+                                <InsertLinkOutlinedIcon />
+                              </IconButton>
+                            </a>
+                          </Tooltip>
                         </div>
                       </div>
                       <div className="row justify-content-evenly align-items-center">
@@ -170,7 +224,6 @@ export default function ItemDetail() {
                               <span
                                 className=" mb-4 p-1"
                                 style={{
-                                  backgroundColor: "#01022e",
                                   color: "rgb(247 220 90)",
 
                                   margin: "0.3rem",
