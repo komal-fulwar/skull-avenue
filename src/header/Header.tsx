0ee5ec833b9e55a831492b40ac991fb519f2840a
Komal Fulwar <47312905+komal-fulwar@users.noreply.github.com>
2022-04-29 16:13:27 +0530

    header icon changed

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -89,7 +89,7 @@ export default function Header({ handleWalletAddress }: HeaderProps) {
   return (
     <>
       <header
-        className="header__1 js-header"
+        className="header__1 js-headern web"
         id="header"
         style={{ minHeight: "181px", backgroundColor: "#26252a" }}
       >
@@ -102,6 +102,234 @@ export default function Header({ handleWalletAddress }: HeaderProps) {
               <a href="/">
                 <img
                   style={{ width: "250px", height: "94px" }}
+                  className="header__logo"
+                  id="logo_js"
+                  src="https://skullavenue.io/wp-content/uploads/2021/12/Schild_02.png"
+                  alt="logo"
+                />
+              </a>
+            </div>
+
+            <div className="header__menu">
+              <ul className="d-flex space-x-20">
+                <li>
+                  {" "}
+                  <a
+                    style={{ fontFamily: "boston" }}
+                    className="color_white"
+                    href="https://skullavenue.io/"
+                  >
+                    {" "}
+                    Home
+                  </a>{" "}
+                </li>
+                <div className="vl"></div>
+                <li>
+                  <a
+                    style={{ fontFamily: "boston" }}
+                    className="color_white"
+                    href="https://skullavenue.io/roadmap/"
+                  >
+                    {" "}
+                    RoadMap
+                  </a>{" "}
+                </li>
+                <div className="vl"></div>
+                <li>
+                  {" "}
+                  <a
+                    style={{ fontFamily: "boston" }}
+                    className="color_white"
+                    href="https://skullavenue.io/"
+                  >
+                    {" "}
+                    Collection
+                  </a>{" "}
+                </li>
+                <div className="vl"></div>
+                <li>
+                  {" "}
+                  <a
+                    style={{ fontFamily: "boston" }}
+                    className="color_white"
+                    href="https://skullavenue.io/attributes/"
+                  >
+                    Attributes
+                  </a>
+                </li>
+                <div className="vl"></div>
+                <li>
+                  {" "}
+                  <a
+                    style={{ fontFamily: "boston" }}
+                    className="color_white"
+                    href="https://skullavenue.io/community/"
+                  >
+                    {" "}
+                    community
+                  </a>{" "}
+                </li>
+              </ul>
+            </div>
+
+            {provider && !walletKey && (
+              <div className="header__btns">
+                <a
+                  style={{
+                    fontSize: "18px",
+                    padding: " 20px 40px",
+                    fill: "black",
+                    color: "black",
+                    backgroundColor: "#f8c307",
+                    fontFamily: "boston",
+                    borderRadius: " 5px",
+                  }}
+                  onClick={connectWallet}
+                >
+                  Connect wallet
+                </a>
+              </div>
+            )}
+            {provider && walletKey && (
+              <div
+                className="header__avatar"
+                style={{
+                  fontSize: "18px",
+                  width: "14rem",
+                  whiteSpace: "nowrap",
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                  display: "inline-block",
+                  fill: "black",
+                  paddingTop: "1rem",
+                  paddingBottom: "1rem",
+                  paddingLeft: "0.4rem",
+                  color: "black",
+                  backgroundColor: "#f8c307",
+                  fontFamily: "boston",
+                  borderRadius: " 5px",
+                }}
+              >
+                <div className=" text-centre">
+                  <span style={{ color: "black" }}>
+                    {provider && walletKey && (
+                      <p style={{ color: "black" }}> {walletKey}</p>
+                    )}
+                  </span>
+                </div>
+
+                <div className="avatar_popup space-y-20">
+                  <div className="d-flex align-items-center justify-content-between">
+                    <span> 13b9ebda035r178... </span>
+                    <a href="/" className="ml-2">
+                      <i className="ri-file-copy-line"></i>
+                    </a>
+                  </div>
+                  <div className="d-flex align-items-center space-x-10">
+                    <img
+                      className="coin"
+                      src="assets/img/logos/coin.svg"
+                      alt="/"
+                    />
+                    <div className="info">
+                      <p className="text-sm font-book text-gray-400">Balance</p>
+                      <p className="w-full text-sm font-bold text-green-500">
+                        16.58 ETH
+                      </p>
+                    </div>
+                  </div>
+                  <div className="hr"></div>
+                  <div className="links space-y-10">
+                    <a href="#">
+                      <i className="ri-landscape-line"></i>{" "}
+                      <span> My items</span>
+                    </a>
+                    <a href="edit_profile.html">
+                      <i className="ri-pencil-line"></i>{" "}
+                      <span> Edit Profile</span>
+                    </a>
+                    <div className="btn" onClick={disconnectWallet}>
+                      <i className="ri-logout-circle-line"></i>{" "}
+                      <span> Disconnect</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
+
+            <div className="header__burger js-header-burger"></div>
+
+            <div className="header__mobile js-header-mobile">
+              <div className="header__mobile__menu space-y-40">
+                <ul className="d-flex space-y-20">
+                  <li>
+                    {" "}
+                    <a className="color_black" href="https://skullavenue.io/">
+                      {" "}
+                      Home
+                    </a>{" "}
+                  </li>
+                  <li>
+                    {" "}
+                    <a className="color_black" href="https://skullavenue.io/">
+                      {" "}
+                      RoadMap
+                    </a>{" "}
+                  </li>
+                  <li>
+                    {" "}
+                    <a className="color_black" href="#">
+                      {" "}
+                      Collection
+                    </a>{" "}
+                  </li>
+                  <li>
+                    {" "}
+                    <a className="color_black" href="https://skullavenue.io/">
+                      {" "}
+                      Attributes
+                    </a>{" "}
+                  </li>
+                  <li>
+                    {" "}
+                    <a className="color_black" href="#">
+                      community
+                    </a>
+                  </li>
+                </ul>
+                {/* <li>
+                <a
+                  className="btn btn-grad btn-sm"
+                  style={{ padding: "15px" }}
+                  href="Connect-wallet.html"
+                >
+                  <i className="ri-wallet-3-line"></i>
+                  Connect wallet
+                </a>
+                <a href="">
+                  <img
+                    width="45"
+                    src="https://phantom.app/apple-touch-icon.png"
+                    alt=""
+                  />
+                </a>
+              </li> */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <header
+        className="header__1 js-header mobile"
+        id="header"
+        style={{ minHeight: "66px", backgroundColor: "#26252a" }}
+      >
+        <div className="container">
+          <div className="wrapper js-header-wrapper">
+            <div className="header__logo">
+              <a href="/">
+                <img
+                  style={{ width: "144px", height: "54px" }}
                   className="header__logo"
                   id="logo_js"
                   src="https://skullavenue.io/wp-content/uploads/2021/12/Schild_02.png"
